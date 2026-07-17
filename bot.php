feb6b9b869f3588ee4adf45bf60019aa76c29d44
asafonov <me@asafonov.org>
2026-07-17 09:29:45 +0000

    minor fix

--- bot.php
+++ bot.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once('config.php');
+require_once('message.php');
 
 function doCronLogic ($input) {
   //@TODO implement cron logic here, if needed
--- config.sample.php
+++ config.sample.php
@@ -2,8 +2,8 @@
 
 define('TOKEN', 'TELEGRAM_API_TOKEN');
 define('PHP_BIN', '/usr/bin/php');
-define('WORKER_PATH', '/var/www/html/weather.bot');
-define('WORKER_CACHE_PATH', '/var/cache/weather.bot');
-define('WORKER_LOG_PATH', '/var/log/weather.bot');
+define('WORKER_PATH', '/var/www/html/telegram.bot');
+define('WORKER_CACHE_PATH', '/var/cache/telegram.bot');
+define('WORKER_LOG_PATH', '/var/log/telegram.bot');
 define('MAX_RETRIES', 3);
 define('REQUEST_TIMEOUT', 8);
